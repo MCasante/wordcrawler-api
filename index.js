31b5bd84c88b58c1333668b132448192d45de911
MCasante <math.casante@gmail.com>
2020-08-22 21:47:52 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 const PORT = 3000
 
+
 const WordCrawler = require('./src/wordCrawler')
 
-
+app.use(cors)
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.send('WordCrawler Api')
 })
